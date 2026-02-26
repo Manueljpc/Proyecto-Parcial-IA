7559d80608eb276ef3c585a2871015822cbd0d27
manueljpc <jeremypilier1@gmail.com>
2026-02-25 20:52:46 -0400

    simplemente creando lo mas basico del enemigo

---
+++ proyecto/jugador.py
@@ -0,0 +1,27 @@
+import pygame
+
+class jugador:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        
+        self.tamaño = 40
+        
+        self.velocidad = 4
+        
+        self.vida = 100
+        
+        self.color = (0.0,100)
+        
+        def movimiento(self,keys):
+            if keys[pygame.K_w]:
+                self.y -= self.velocidad
+            if keys[pygame.K_s]:
+                self.y += self.velocidad
+            if keys[pygame.K_a]:     
+                self.x -= self.velocidad
+            if keys[pygame.K_d]:
+                self.x += self.velocidad
+                
+        def draw(self,screen):
+            pygame.draw.rect(screen,self.color,(self.x,self.y,self.tamaño,self.tamaño))             
